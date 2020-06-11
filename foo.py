d704bc69d6b1891a184852792135f9e403f57542
Junshan Bao <baojunshan123@gmail.com>
2020-06-12 00:40:07 +0800

    finish datasets

--- foo.py
+++ foo.py
@@ -1,36 +1,55 @@
-class A:
-    def __init__(self, a):
-        self.a = a
+# class A:
+#     def __init__(self, a):
+#         self.a = a
+#
+#     def kk(self):
+#         return 1
+#
+#     def foo(self, **params):
+#         for i, j in params.items():
+#             print(i, j)
+#             self.__dict__[i] = j
+#         print(self.a)
+#
+#
+# aa = A(1)
+#
+# d = {"a", 5}
+# aa.foo(a=5)
+#
+# import pandas as pd
+# import numpy as np
+#
+# a = pd.DataFrame({"a": [1, 2]})
+# print(np.array(a))
+#
+# x = [[2, 4.], [2, 4.]]
+# b = np.square(np.sqrt(x))
+# print(np.array(b).round(12))
+# print(b.round(12) == x)
+#
+# from stlearn import ab
+# import os, sys
+# print(os.getcwd())
 
-    def kk(self):
-        return 1
+from sklearn.datasets import load_breast_cancer, load_boston, load_wine
+# import pickle
+#
+# df = load_wine()
+# # print(df)
+#
+# with open("wine.pkl", "wb") as f:
+#     pickle.dump(df, f)
+#
+# with open("wine.pkl", "rb") as f:
+#     df = pickle.load(f)
+# print(df.keys())
 
-    def foo(self, **params):
-        for i, j in params.items():
-            print(i, j)
-            self.__dict__[i] = j
-        print(self.a)
+from sklearn.datasets import load_iris
 
 
-aa = A(1)
-
-d = {"a", 5}
-aa.foo(a=5)
-
-import pandas as pd
-import numpy as np
-
-a = pd.DataFrame({"a": [1, 2]})
-print(np.array(a))
-
-x = [[2, 4.], [2, 4.]]
-b = np.square(np.sqrt(x))
-print(np.array(b).round(12))
-print(b.round(12) == x)
-
-from stlearn import ab
-import os, sys
-print(os.getcwd())
+data = load_iris(True)
+print(data)
 
 
 
---
+++ stlearn/datasets/__init__.py
@@ -0,0 +1,35 @@
+import pickle
+from pathlib import PurePath
+
+__all__ = [
+    "load_iris",
+    "load_boston",
+    "load_breast_cancer",
+    "load_wine"
+]
+
+CURR_FOLDER_PATH = PurePath(__file__).parent
+
+
+def _load_data(filename, x_y):
+    with open(filename, "rb") as f:
+        data = pickle.load(f)
+    if not x_y:
+        return data
+    return data["data"], data["target"]
+
+
+def load_iris(x_y=False):
+    return _load_data(str(CURR_FOLDER_PATH / "iris.pkl"), x_y)
+
+
+def load_boston(x_y=False):
+    return _load_data(str(CURR_FOLDER_PATH / "boston.pkl"), x_y)
+
+
+def load_breast_cancer(x_y=False):
+    return _load_data(str(CURR_FOLDER_PATH / "breast_cancer.pkl"), x_y)
+
+
+def load_wine(x_y=False):
+    return _load_data(str(CURR_FOLDER_PATH / "wine.pkl"), x_y)
