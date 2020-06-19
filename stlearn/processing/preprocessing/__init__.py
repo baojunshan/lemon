a8c81f75a27eee70050e3da2176148c92d28ac4a
Junshan Bao <junshanbao@JunshandeMacBook-Pro.local>
2020-06-19 19:01:16 +0800

    change comp

--- stlearn/processing/preprocessing/__init__.py
+++ stlearn/processing/preprocessing/__init__.py
@@ -2,3 +2,6 @@ from ._binarizer import Binarizer
 from ._function_transformer import FunctionTransformer
 from ._kbins_discretizer import KBinsDiscretizer
 from ._onehot_encoder import OneHotEncoder
+from ._label_binarizer import LabelBinarizer
+from ._label_encoder import LabelEncoder
+from ._max_abs_scaler import MaxAbsScaler
---
+++ stlearn/processing/preprocessing/_max_abs_scaler.py
@@ -0,0 +1,15 @@
+from stlearn.base import BasePreprocessor
+
+
+class MaxAbsScaler(BasePreprocessor):
+    def __init__(self):
+        pass
+
+    def fit(self, x):
+        pass
+
+    def transform(self, x):
+        pass
+
+    def fit_transform(self, x):
+        pass
